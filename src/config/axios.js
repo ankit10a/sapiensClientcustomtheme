8a0da29273a5637acdaacdc084d0ee758445cb2b
ankit10a <ankitverma6440@gmail.com>
2022-07-06 17:55:08 +0530

    change server ulrl

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { userAuth } from '../utils/userAuth';
-const server = process.env.server || "https://sub.deeponlineseller.com/api/v1";
+const server = process.env.server || `http://sapiensserver-env.eba-8j3jdhih.us-east-1.elasticbeanstalk.com/`
+// "https://sub.deeponlineseller.com/api/v1";
 
 let token = userAuth();
 export const serverV1Instance = axios.create({
-    baseURL: server,
+    baseURL: `${server}/api/v1`,
     withCredentials: true,
     headers: { 'authorization': token || "" }
 })
