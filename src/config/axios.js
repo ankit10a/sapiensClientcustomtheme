fcd47315c52af3243af8ad2bd27612f637c2c350
ankit10a <ankitverma6440@gmail.com>
2022-07-06 17:56:00 +0530

    url fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { userAuth } from '../utils/userAuth';
-const server = process.env.server || `http://sapiensserver-env.eba-8j3jdhih.us-east-1.elasticbeanstalk.com/`
+const server = process.env.server || `http://sapiensserver-env.eba-8j3jdhih.us-east-1.elasticbeanstalk.com`
 // "https://sub.deeponlineseller.com/api/v1";
 
 let token = userAuth();
